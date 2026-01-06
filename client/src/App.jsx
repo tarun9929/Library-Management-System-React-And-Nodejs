532aa079146a3ed3b4c522b8666dd3c30f98e527
Tarun DIdwaniya <89730122+tarun9929@users.noreply.github.com>
2026-01-06 08:34:34 +0000

    feat(React Setup) : setup react frontend app with tailwindcss

---
+++ client/src/App.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import { FaCartShopping } from "react-icons/fa6";
+import { FaHistory } from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
+
+function App() {
+  const [books, setBooks] = React.useState(Array(30).fill({
+    id: '1152424194',
+    name: "Harry Potter and the Philosopher's Stone",
+    auther: 'J.K. Rowling',
+    price: 259,
+    pages: 200,
+    availability: '50 PCS'
+  }));
+
+  return (
+    <>
+      <div className="w-fit sm:w-[80%] mx-auto py-6 shadow-2xl mt-6">
+        {/* title  */}
+        <h1 className="text-3xl text-center font-display">
+          Library Management System
+        </h1>
+
+        <div className="px-6 mt-3 flex gap-8 justify-between">
+          <div className='flex gap-3 justify-center items-center'>
+            <div>
+              <button className='border flex gap-2 items-center text-sm px-2 py-1 rounded-md cursor-pointer bg-primary text-white font-bold'>
+                <FaHome />
+                <h2>Home</h2>
+              </button>
+            </div>
+            <div>
+              <button className='border flex gap-2 items-center text-sm px-2 py-1 rounded-md cursor-pointer bg-primary text-white font-bold'>
+                <FaHistory />
+                <h2>Recent Books</h2>
+              </button>
+            </div>
+          </div>
+         <div>
+              <button className='border flex gap-2 items-center text-sm px-2 py-1 rounded-md cursor-pointer bg-primary text-white font-bold'>
+                <FaCartShopping />
+                <h2>Cart</h2>
+              </button>
+            </div>
+        </div>
+
+        <div className="px-6 max-h-112.5 overflow-auto">
+          <table className="w-full border-collapse border-2 mt-4">
+            <tr className="font-body text-left bg-secondary text-white">
+              <th className="py-1 px-3"></th>
+              <th className="py-1 px-3">SR.</th>
+              <th className="py-1 px-3">Book ID</th>
+              <th className="py-1 px-3">Book Name</th>
+              <th className="py-1 px-3">Auther</th>
+              <th className="py-1 px-3">Price</th>
+              <th className="py-1 px-3">Pages</th>
+              <th className="py-1 px-3">Availability</th>
+            </tr>
+            
+            {
+              books.map((book , index) => (
+                <tr key={book.id} className="font-body border-t-2 hover:bg-gray-200 cursor-pointer">
+                  <td className="py-1 px-3">
+                    <input type="checkbox" />
+                  </td>
+                  <td className="py-1 px-3">{index + 1}</td>
+                  <td className="py-1 px-3">{book.id}</td>
+                  <td className="py-1 px-3">{book.name}</td>
+                  <td className="py-1 px-3">{book.auther}</td>
+                  <td className="py-1 px-3">₹ {book.price}</td>
+                  <td className="py-1 px-3">{book.pages}</td>
+                  <td className="py-1 px-3">{book.availability}</td>
+                </tr>
+              ))
+            }
+          </table>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App

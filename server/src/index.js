d978b84f4837d6b5867e7df733e8616d514a1e4f
Tarun DIdwaniya <89730122+tarun9929@users.noreply.github.com>
2026-01-06 07:52:24 +0000

    feat(cors): setup cors in project

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,21 @@
 import app from "./app.js";
+import cors from 'cors';
 
 const PORT = process.env.PORT || 3000;
+
+const whiteList = [];
+
+app.use(cors({
+    origin: (origin , callback) => {
+        if(whiteList.indexOf(origin) !== -1) {
+            callback(null , true);
+        } else {
+            callback(null , false);
+        }
+    },
+    credentials: true,
+    allowedHeaders: ['POST' , 'PATCH' , 'DELETE' , 'GET' , 'OPTIONS']
+}))
 
 app.listen(PORT , () => {
     console.log(`🚀 Server is running at http://localhost:${PORT}`);
